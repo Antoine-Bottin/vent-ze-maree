bae9095f9cc1b544dc2804740b2328e01c55906e
antoine-bottin <bttn.atn@gmail.com>
2025-07-24 16:27:26 +0200

    feat(button): react native difference with react

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,8 @@
 import { SignOutButton } from '@/components/SignOutButton'
 import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router'
-import { ImageBackground, Text, View } from 'react-native'
+import { ImageBackground, Pressable, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import globalStyles from './styles/globalStyles'
 
 export default function Page() {
@@ -10,32 +11,46 @@ export default function Page() {
   const picture = require('../assets/images/auth-background.png')
 
   return (
-    <View style={{ flex: 1, width: '100%', height: '100%' }}>
-      <ImageBackground
-        source={picture}
-        style={{
-          flex: 1,
-          width: '100%',
-          height: '100%',
-        }}
-      >
-        <View style={globalStyles.container}>
-          <SignedIn>
-            <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-            <SignOutButton />
-          </SignedIn>
-          <SignedOut>
-            <View style={{ display: 'flex', flexDirection: 'column', gap: 50 }}>
-              <Link href="/(auth)/sign-in">
-                <Text style={globalStyles.signInButton}>Sign in</Text>
-              </Link>
-              <Link href="/(auth)/sign-up">
-                <Text style={globalStyles.signUpButton}>Sign up</Text>
-              </Link>
-            </View>
-          </SignedOut>
-        </View>
-      </ImageBackground>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <ImageBackground
+          source={picture}
+          style={{
+            flex: 1,
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <View style={globalStyles.container}>
+            <SignedIn>
+              <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+              <SignOutButton />
+            </SignedIn>
+            <SignedOut>
+              <View
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  width: '100%',
+                  alignItems: 'center',
+                }}
+              >
+                <Pressable style={globalStyles.signInButton}>
+                  <Link href="/(auth)/sign-in">
+                    <Text>Sign in </Text>
+                  </Link>
+                </Pressable>
+                <View style={{ height: 50 }} />
+                <Pressable style={globalStyles.signUpButton}>
+                  <Link href="/(auth)/sign-up">
+                    <Text>Sign up</Text>
+                  </Link>
+                </Pressable>
+              </View>
+            </SignedOut>
+          </View>
+        </ImageBackground>
+      </View>
+    </SafeAreaView>
   )
 }
